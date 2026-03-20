781210db5639ed987aa49c4be003549154e09fe0
mlrodya2006 <mlrodya2006@gmail.com>
2026-03-20 23:15:51 +0100

    [LAB_4]

--- Django/polls/admin.py
+++ Django/polls/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Choice, Question
 
-# Register your models here.
+# Це дозволить додавати 3 варіанти відповіді відразу під питанням
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Data publikacji", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
--- Django/polls/models.py
+++ Django/polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField("Treść pytania", max_length=200)
+    pub_date = models.DateTimeField("Data publikacji")
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField("Treść odpowiedzi", max_length=200)
+    votes = models.IntegerField("Liczba głosów", default=0)
+
+    def __str__(self):
+        return self.choice_text
